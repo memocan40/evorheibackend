2f6923c7688e45602b1a66d7ec5dd8804e9fe624
memo40 <memocann4040@gmail.com>
2021-02-16 08:36:03 +0100

    process.env setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {Pool}  = require('pg');
 const app     = express();
-const port      =3001;
+const port = process.env.PORT || 3000
 
 
 const dotenv=require('dotenv');
