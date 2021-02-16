7314a835ae756b5f761b5b06b52980a715647445
memo40 <memocann4040@gmail.com>
2021-02-16 11:19:32 +0100

    addet cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const {Pool}  = require('pg');
 const app     = express();
+const cors    = require("cors");
 const port = process.env.PORT || 3000
 
 
 const dotenv=require('dotenv');
 dotenv.config();
+app.use(cors());
 
 const alldata=require("./routes/alldata")
 
